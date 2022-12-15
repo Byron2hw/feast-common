25948ce79e10c07e44b4d0559787ebddadd1f7f2
Byron <Byron@kupotech.com>
2022-12-15 18:23:45 +0800

    remove deploy

--- jar/feast-starter/src/main/java/com/feast/common/exception/CommonHttpStatus.java
+++ jar/feast-starter/src/main/java/com/feast/common/exception/CommonHttpStatus.java
@@ -10,7 +10,7 @@ import org.springframework.http.HttpStatusCode;
  */
 @Getter
 @AllArgsConstructor
-public enum CommonHttpStatus implements HttpStatusCode {
+public enum CommonHttpStatus {
     CACHE_FAILED(1000, Series.CACHE_ERROR, "Cache Failed"),
     DATA_ACCESS_FAILED(1001, Series.DATA_ACCESS_ERROR, "Data Access Failed"),
     MQ_FAILED(1002, Series.MQ_ERROR, "Message Queue Failed"),
@@ -23,45 +23,6 @@ public enum CommonHttpStatus implements HttpStatusCode {
     private final int value;
     private final Series series;
     private final String reasonPhrase;
-
-    public Series series() {
-        return this.series;
-    }
-
-    @Override
-    public int value() {
-        return this.value;
-    }
-
-    @Override
-    public boolean is1xxInformational() {
-        return false;
-    }
-
-    @Override
-    public boolean is2xxSuccessful() {
-        return false;
-    }
-
-    @Override
-    public boolean is3xxRedirection() {
-        return false;
-    }
-
-    @Override
-    public boolean is4xxClientError() {
-        return this.series() == Series.PARAMETER_ERROR;
-    }
-
-    @Override
-    public boolean is5xxServerError() {
-        return this.series() != Series.PARAMETER_ERROR;
-    }
-
-    @Override
-    public boolean isError() {
-        return true;
-    }
 
     @Getter
     public static enum Series {
