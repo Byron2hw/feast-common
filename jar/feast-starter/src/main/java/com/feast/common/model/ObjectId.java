77f5b89c52ee0cdaa76d89b59b64c7e5221d7cec
Byron <Byron@kupotech.com>
2023-07-26 21:06:03 +0800

    pom+ObjectId

--- jar/feast-starter/src/main/java/com/feast/common/model/ObjectId.java
+++ jar/feast-starter/src/main/java/com/feast/common/model/ObjectId.java
@@ -4,76 +4,103 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.lang.management.ManagementFactory;
 import java.net.NetworkInterface;
+import java.nio.BufferUnderflowException;
 import java.nio.ByteBuffer;
 import java.security.SecureRandom;
 import java.util.Enumeration;
-import java.util.concurrent.atomic.LongAdder;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * @author Byron
  * @date 2022/12/16 3:34 下午
  */
 @Slf4j
-public record ObjectId(int machineId, short processId,
-                       LongAdder adder) implements Comparable<ObjectId> {
-    private static final char[] CHARS = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+public class ObjectId {
     /**
-     * 2<sup>24</sup>-1.
+     * 2<sup>24</sup> - 1
      */
-    private static final int MAX_VALUE = 16777215;
-    private static final int LENGTH = 24;
-    private static final int SHIFT = 16;
-    private static final int BIT_SHIFT = 8;
+    private static final int LOW_ORDER_THREE_BYTES = 0xffffff;
+    private static final long MACHINE_IDENTIFIER;
+    private static final short PROCESS_IDENTIFIER;
+    private static final AtomicLong NEXT_COUNTER = new AtomicLong((new SecureRandom()).nextLong());
+    private static final char[] HEX_CHARS = new char[]{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+    private final long timestamp;
+    private final long machineIdentifier;
+    private final short processIdentifier;
+    private final long counter;
 
     public static ObjectId get() {
-        LongAdder longAdder = new LongAdder();
-        long randomValue = new SecureRandom().nextLong();
-        longAdder.add(randomValue & MAX_VALUE);
-        return new ObjectId(getMachineId(), getProcessId(), longAdder);
+        return new ObjectId();
     }
 
-    public String id() {
-        char[] chars = new char[LENGTH];
-        byte[] bytes = this.toByte();
-        int length = CHARS.length - 1;
-        int i = 0;
-        for (byte aByte : bytes) {
-            chars[i++] = CHARS[aByte >> 4 & length];
-            chars[i++] = CHARS[aByte & length];
+    public static String getString() {
+        return (new ObjectId()).toHexString();
+    }
+
+    public static boolean isValid(final String hexString) {
+        if (hexString == null) {
+            throw new IllegalArgumentException();
+        } else {
+            int len = hexString.length();
+            if (len != 24) {
+                return false;
+            } else {
+                for(int i = 0; i < len; ++i) {
+                    char c = hexString.charAt(i);
+                    if ((c < '0' || c > '9') && (c < 'a' || c > 'f') && (c < 'A' || c > 'F')) {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
         }
-        return new String(chars);
     }
 
-    /**
-     *
-     * @return id
-     */
-    public static String getId() {
-        return get().id();
+    public ObjectId() {
+        this(System.currentTimeMillis());
     }
 
-    private byte[] toByte() {
-        int capacity = LENGTH >> 1;
-        ByteBuffer buffer = ByteBuffer.allocate(capacity);
-        long now = System.currentTimeMillis();
-        buffer.put((byte)(now >> LENGTH));
-        buffer.put((byte)(now >> SHIFT));
-        buffer.put((byte)(now >> BIT_SHIFT));
-        buffer.put((byte)(now));
-        buffer.put((byte)(machineId >> SHIFT));
-        buffer.put((byte)(machineId >> BIT_SHIFT));
-        buffer.put((byte)(machineId));
-        buffer.put((byte) (processId >> BIT_SHIFT));
-        buffer.put((byte) (processId));
-        adder.increment();
-        long adderValue = adder.longValue();
-        buffer.put((byte)(adderValue >> SHIFT));
-        buffer.put((byte)(adderValue >> BIT_SHIFT));
-        buffer.put((byte)(adderValue));
+    public ObjectId(final Long mills) {
+        this(mills, MACHINE_IDENTIFIER, PROCESS_IDENTIFIER, NEXT_COUNTER.getAndIncrement(), false);
+    }
+    private ObjectId(final long timestamp, final long machineIdentifier, final short processIdentifier, final long counter, final boolean checkCounter) {
+        if ((machineIdentifier & -16777216) != 0) {
+            throw new IllegalArgumentException("The machine identifier must be between 0 and 16777215 (it must fit in three bytes).");
+        } else if (checkCounter && (counter & -16777216) != 0) {
+            throw new IllegalArgumentException("The counter must be between 0 and 16777215 (it must fit in three bytes).");
+        } else {
+            this.timestamp = timestamp;
+            this.machineIdentifier = machineIdentifier;
+            this.processIdentifier = processIdentifier;
+            this.counter = counter & LOW_ORDER_THREE_BYTES;
+        }
+    }
+
+    public byte[] toByteArray() {
+        ByteBuffer buffer = ByteBuffer.allocate(12);
+        this.putToByteBuffer(buffer);
         return buffer.array();
     }
 
-    private static int getMachineId() {
+    public void putToByteBuffer(final ByteBuffer buffer) {
+        notNull("buffer", buffer);
+        isTrueArgument("buffer.remaining() >=12", buffer.remaining() >= 12);
+        buffer.put(long3(this.timestamp));
+        buffer.put(long2(this.timestamp));
+        buffer.put(long1(this.timestamp));
+        buffer.put(long0(this.timestamp));
+        buffer.put(long2(this.machineIdentifier));
+        buffer.put(long1(this.machineIdentifier));
+        buffer.put(long0(this.machineIdentifier));
+        buffer.put(short1(this.processIdentifier));
+        buffer.put(short0(this.processIdentifier));
+        buffer.put(long2(this.counter));
+        buffer.put(long1(this.counter));
+        buffer.put(long0(this.counter));
+    }
+
+    private static int createMachineIdentifier() {
         int machinePiece;
         try {
             StringBuilder sb = new StringBuilder();
@@ -85,9 +112,12 @@ public record ObjectId(int machineId, short processId,
                 byte[] mac = ni.getHardwareAddress();
                 if (mac != null) {
                     ByteBuffer bb = ByteBuffer.wrap(mac);
-                    sb.append(bb.getChar());
-                    sb.append(bb.getChar());
-                    sb.append(bb.getChar());
+                    try {
+                        sb.append(bb.getChar());
+                        sb.append(bb.getChar());
+                        sb.append(bb.getChar());
+                    } catch (BufferUnderflowException var7) {
+                    }
                 }
             }
 
@@ -97,10 +127,11 @@ public record ObjectId(int machineId, short processId,
             log.warn("Failed to get machine identifier from network interface, using random number instead", var8);
         }
 
-        return machinePiece & MAX_VALUE;
+        machinePiece &= LOW_ORDER_THREE_BYTES;
+        return machinePiece;
     }
 
-    private static short getProcessId() {
+    private static short createProcessIdentifier() {
         short processId;
         try {
             String processName = ManagementFactory.getRuntimeMXBean().getName();
@@ -117,21 +148,60 @@ public record ObjectId(int machineId, short processId,
         return processId;
     }
 
-    @Override
-    public int compareTo(final ObjectId other) {
-        if (other == null) {
-            throw new NullPointerException();
+    private static byte long3(final long x) {
+        return (byte)(x >> 32);
+    }
+    private static byte long2(final long x) {
+        return (byte)(x >> 16);
+    }
+    private static byte long1(final long x) {
+        return (byte)(x >> 8);
+    }
+    private static byte long0(final long x) {
+        return (byte)(x);
+    }
+
+    private static byte short1(final short x) {
+        return (byte)(x >> 8);
+    }
+
+    private static byte short0(final short x) {
+        return (byte)(x);
+    }
+
+    private static <T> T notNull(String name, T value) {
+        if (value == null) {
+            throw new IllegalArgumentException(name + " can not be null");
         } else {
-            byte[] byteArray = this.toByte();
-            byte[] otherByteArray = other.toByte();
+            return value;
+        }
+    }
+    public String toHexString() {
+        char[] chars = new char[24];
+        int i = 0;
+        byte[] var3 = this.toByteArray();
+        int var4 = var3.length;
 
-            for(int i = 0; i < 12; ++i) {
-                if (byteArray[i] != otherByteArray[i]) {
-                    return (byteArray[i] & 255) < (otherByteArray[i] & 255) ? -1 : 1;
-                }
-            }
+        for(int var5 = 0; var5 < var4; ++var5) {
+            byte b = var3[var5];
+            chars[i++] = HEX_CHARS[b >> 4 & 15];
+            chars[i++] = HEX_CHARS[b & 15];
+        }
+        return new String(chars);
+    }
 
-            return 0;
+    private static void isTrueArgument(String name, boolean condition) {
+        if (!condition) {
+            throw new IllegalArgumentException("state should be: " + name);
+        }
+    }
+
+    static {
+        try {
+            MACHINE_IDENTIFIER = createMachineIdentifier();
+            PROCESS_IDENTIFIER = createProcessIdentifier();
+        } catch (Exception var1) {
+            throw new RuntimeException(var1);
         }
     }
 }
